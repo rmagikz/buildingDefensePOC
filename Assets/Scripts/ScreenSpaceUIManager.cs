a05a18c8e13ab61938c3cb7d5e00420533b3b46e
rmagikz <abriftwr@gmail.com>
2022-11-10 17:02:52 -0500

    Adding UI elements

---
+++ Assets/Scripts/ScreenSpaceUIManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class ScreenSpaceUIManager : MonoBehaviour
+{
+    [SerializeField] private WorldSpaceUIManager worldSpaceUIManager;
+    [SerializeField] private CameraManager cameraManager;
+    [SerializeField] private Button upgrade1, upgrade2, backButton;
+
+    private bool panelEnabled = true;
+
+    void Start() {
+        Toggle();
+
+        backButton.onClick.AddListener(BackClicked);
+        upgrade1.onClick.AddListener(Upgrade1Clicked);
+        upgrade2.onClick.AddListener(Upgrade2Clicked);
+    }
+    
+    public void Toggle() {
+        if (panelEnabled) {
+            panelEnabled = false;
+            backButton.transform.DOScale(new Vector3(0,0,0), 0.5f);
+            upgrade1.transform.DOScale(new Vector3(0,0,0), 0.5f);
+            upgrade2.transform.DOScale(new Vector3(0,0,0), 0.5f);
+        } else {
+            panelEnabled = true;
+            backButton.transform.DOScale(new Vector3(1,1,1), 0.5f);
+            upgrade1.transform.DOScale(new Vector3(1,1,1), 0.5f);
+            upgrade2.transform.DOScale(new Vector3(1,1,1), 0.5f);
+        }
+    }
+
+    private void BackClicked() {
+        Toggle();
+        worldSpaceUIManager.ToggleAll();
+        cameraManager.LookAtBuilding();
+    }
+
+    private void Upgrade1Clicked() {
+
+    }
+
+    private void Upgrade2Clicked() {
+
+    }
+}
--- Assets/Scripts/WorldSpaceUIManager.cs
+++ Assets/Scripts/WorldSpaceUIManager.cs
@@ -2,17 +2,66 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class WorldSpaceUIManager : MonoBehaviour
 {
     [SerializeField] private Button barracks, logistics, engineering, misc;
     [SerializeField] private CameraManager cameraManager;
     [SerializeField] private BuildingManager buildingManager;
+    [SerializeField] private ScreenSpaceUIManager screenSpaceUIManager;
+
+    private bool buttonsEnabled = true;
 
     void Start() {
-        barracks.onClick.AddListener(() => cameraManager.LookAtRoom(buildingManager.barracks));
-        logistics.onClick.AddListener(() => cameraManager.LookAtRoom(buildingManager.logistics));
-        engineering.onClick.AddListener(() => cameraManager.LookAtRoom(buildingManager.engineering));
-        misc.onClick.AddListener(() => cameraManager.LookAtRoom(buildingManager.misc));
+        barracks.onClick.AddListener(BarracksClicked);
+        logistics.onClick.AddListener(LogisticsClicked);
+        engineering.onClick.AddListener(EngineeringClicked);
+        misc.onClick.AddListener(MiscClicked);
+    }
+
+    public void Toggle(Button p) {
+        if (p.enabled) {
+            p.enabled = false;
+            p.transform.DOScale(new Vector3(0,0,0), .5f);
+        } else {
+            p.enabled = true;
+            p.transform.DOScale(new Vector3(1,1,1), .5f);
+        }
+    }
+
+    public void ToggleAll() {
+        Toggle(barracks);
+        Toggle(logistics);
+        Toggle(engineering);
+        Toggle(misc);
+    }
+
+    private void BarracksClicked() {
+        cameraManager.LookAtRoom(buildingManager.barracks);
+        //Toggle(barracks);
+        ToggleAll();
+        screenSpaceUIManager.Toggle();
+    }
+
+    private void LogisticsClicked() {
+        cameraManager.LookAtRoom(buildingManager.logistics);
+        //Toggle(logistics);
+        ToggleAll();
+        screenSpaceUIManager.Toggle();
+    }
+
+    private void EngineeringClicked() {
+        cameraManager.LookAtRoom(buildingManager.engineering);
+        //Toggle(engineering);
+        ToggleAll();
+        screenSpaceUIManager.Toggle();
+    }
+
+    private void MiscClicked() {
+        cameraManager.LookAtRoom(buildingManager.misc);
+        //Toggle(misc);
+        ToggleAll();
+        screenSpaceUIManager.Toggle();
     }
 }
